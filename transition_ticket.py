d91f3163a332a286f9491e7ff5966ddf53de470e
Vivek Pawar <VPawar@b2x.com>
2018-06-25 16:48:13 +0530

    input options in new line

--- transition_ticket.py
+++ transition_ticket.py
@@ -13,7 +13,7 @@ projects = jira.projects()
 ticketName = str(input("Enter the ticket name: "))
 issue = jira.issue(ticketName)
 
-command = str(input("Enter option for following task: 1. Get a story into current sprint 2. Move Story 3. Update Story Points 4. Read Story's Description & comments 5. Add work log to specific ticket "))
+command = str(input("Enter option for following task: \n 1. Get a story into current sprint \n 2. Move Story \n 3. Update Story Points \n 4. Read Story's Description & comments \n 5. Add work log to specific ticket \n 6. Assign Ticket To Me \n "))
 
 if command == "1":
     boards = jira.boards()
@@ -22,30 +22,34 @@ if command == "1":
     sprints = jira.sprints(board_id)
     # for sprint in sprints:
     #     print('%s: %s ' % (sprint.id, sprint.name))
-    sprintId = sprints[-1].id
-    # print(sprints[-1].name)
-    # jira.add_issues_to_sprint(220, sprintId)
-    # jira.assign_issue(issue.id, username)
-    # storyPoints = int(input("Enter story points: "))
-    # issue.update(fields={'customfield_10021': storyPoints})         # --> Story Points
-    # issue.update(fields={'customfield_10904': {'id': "11568"}})     # --> Team
+    sprintId = int(sprints[-1].id)
+    issueId = str(issue.id)
+    issueIdList = list()
+    issueIdList.append(issueId)    
+    # print(issue.id)
+    # print("issue key ", issueIdList)
+    jira.assign_issue(issue.id, username)
+    jira.add_issues_to_sprint(sprintId, issueIdList)
+    storyPoints = int(input("Enter story points: "))
+    issue.update(fields={'customfield_10021': storyPoints})         # --> Story Points
+    issue.update(fields={'customfield_10904': {'id': "11568"}})     # --> Team
 elif command == "2":
-    print(issue.id)
+    # print(issue.id)
 
-    # transitions = jira.transitions(issue)
-    # ticketStatus = str(input("What you want to do with it: "))
-    #
-    # if ticketStatus != "inprogress" and ticketStatus != "resolve":
-    #     print("Please enter proper status")
-    # else:
-    #     print([(t['id'], t['name']) for t in transitions])
-    #     # print(issue.fields.status)
-    #     if ticketStatus == "inprogress":
-    #         jira.transition_issue(issue, '4')
-    #     elif ticketStatus == "resolve":
-    #         jira.transition_issue(issue, '5')
-    #     else:
-    #         print("Something went wrong!!")
+    transitions = jira.transitions(issue)
+    ticketStatus = str(input("What you want to do with it: \n 1. Inprogress \n 2. Resolve \n"))
+    
+    if ticketStatus != "1" and ticketStatus != "2":
+        print("Please enter proper status")
+    else:
+        # print([(t['id'], t['name']) for t in transitions])
+        # print(issue.fields.status)
+        if ticketStatus == "1":
+            jira.transition_issue(issue, '4')
+        elif ticketStatus == "2":
+            jira.transition_issue(issue, '5')
+        else:
+            print("Please select option either 1 or 2")
 
 
     # issues = jira.search_issues('project=PP')
@@ -60,6 +64,7 @@ elif command == "3":
     # print(storyPoints)
     issue.update(fields={'customfield_10021': storyPoints})
 elif command == "4":
+    print("========================================================================")
     print("Description ")
     print(issue.raw['fields']['description'])
     print("========================================================================")
@@ -67,7 +72,8 @@ elif command == "4":
     print(issue.fields.comment.comments)
 elif command == "5":
     timeSpent = str(input("Please enter the time you want to log (eg. 3w 4d 12h 30m): "))
-    jira.add_worklog(issue, timeSpent=timeSpent, timeSpentSeconds=None, adjustEstimate=None, newEstimate=None,
-                     reduceBy=None, comment=None, started=None, user=None)
+    jira.add_worklog(issue, timeSpent=timeSpent, timeSpentSeconds=None, adjustEstimate=None, newEstimate=None, reduceBy=None, comment=None, started=None, user=None)
+elif command == "6":
+    jira.assign_issue(issue.id, username)
 else:
     print("Invalid input")
